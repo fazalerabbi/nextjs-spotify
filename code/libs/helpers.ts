07c2ffa067edbd829cb529e6cb4c82988f7147f9
Fazal E Rabbi <fazalerabbi@Fazals-MacBook-Pro.local>
2023-07-12 17:30:05 +0500

    Update the URL

--- code/libs/helpers.ts
+++ code/libs/helpers.ts
@@ -1,10 +1,7 @@
 import { Price } from "@/types";
 
 export const getURL = () => {
-  let url =
-    process.env.NEXT_PUBLIC_SUPABASE_URL ??
-    process.env.NEXT_PUBLIC_VERCEL_URL ??
-    "http://localhost:4200/";
+  let url = process.env.NEXT_PUBLIC_VERCEL_URL ?? "http://localhost:4200/";
 
   url = url.includes("http") ? url : `https://${url}`;
 
